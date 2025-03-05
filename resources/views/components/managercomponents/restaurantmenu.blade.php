fea4caa3c543e30a32b33ae7aa32d9e6f8b15f16
knached99 <khalednached@gmail.com>
2025-03-05 17:31:24 -0500

    Added dark mode support for the forms

--- resources/views/components/managercomponents/restaurantmenu.blade.php
+++ resources/views/components/managercomponents/restaurantmenu.blade.php
@@ -6,23 +6,23 @@
         <table class="min-w-full divide-y divide-gray-200">
           <thead>
             <tr>
-              <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Section Name</th>
-              <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Created At</th>
-              <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Updated At</th>
-              <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">#Menu Items</th>
-              <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">View</th>
-              <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Delete</th>
+              <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-white uppercase">Section Name</th>
+              <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-white uppercase">Created At</th>
+              <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-white uppercase">Updated At</th>
+              <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-white uppercase">#Menu Items</th>
+              <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-white uppercase">View</th>
+              <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-white uppercase">Delete</th>
             </tr>
           </thead>
           <tbody class="divide-y divide-gray-200">
             @foreach($sections as $section)
             <tr>
-              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">{{$section->name}}</td>
-              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">{{{date('F jS, Y \a\t g:i A', strtotime($section->created_at))}}}</td>
-              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">{{{date('F jS, Y \a\t g:i A', strtotime($section->updated_at))}}}</td>
-              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">{{$section->menuItems->count()}}</td> <!-- Displaying number of menu items -->
-              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">View</td>
-              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">
+              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-white">{{$section->name}}</td>
+              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-white">{{{date('F jS, Y \a\t g:i A', strtotime($section->created_at))}}}</td>
+              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-white">{{{date('F jS, Y \a\t g:i A', strtotime($section->updated_at))}}}</td>
+              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-white">{{$section->menuItems->count()}}</td> <!-- Displaying number of menu items -->
+              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-white">View</td>
+              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-white">
               <form action="{{route('deleteSection', ['sectionID' => $section->menu_section_id])}}" method="post">
               @csrf 
               @method('DELETE')
--- resources/views/livewire/forms/multi-step-form.blade.php
+++ resources/views/livewire/forms/multi-step-form.blade.php
@@ -1,21 +1,21 @@
-<div class="max-w-lg mx-auto p-4 bg-white shadow-lg rounded-md">
+<div class="max-w-lg mx-auto p-4 bg-dark shadow-lg rounded-md">
     <form wire:submit.prevent="nextStep">
         <!-- Step 1: Create Section -->
         @if ( $step == 1)
             <div class="mb-4">
-                <label for="sectionName" class="block text-sm font-medium text-gray-700">Section Name</label>
-                <input type="text" id="sectionName" wire:model="sectionName" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm" style="@error('sectionName') border-color: red; @enderror"/>
+                <label for="sectionName" class="block text-sm font-medium text-white">Section Name</label>
+                <input type="text" id="sectionName" wire:model="sectionName" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm" style="background-color: #000; @error('sectionName') border-color: red; @enderror"/>
                 @error('sectionName') <span style="color: red;">{{ $message }}</span> @enderror
             </div>
 
             <div class="mb-4">
-                <label class="block text-sm font-medium text-gray-700">Do you want to add menu items to this section?</label>
+                <label class="block text-sm font-medium text-white">Do you want to add menu items to this section?</label>
                 <label class="inline-flex items-center">
-                    <input type="radio" wire:model="addMenuItems" value="yes" class="form-radio text-blue-500" style="@error('addMenuItems') border-color: red; @enderror"/>
+                    <input type="radio" wire:model="addMenuItems" value="yes" class="form-radio text-blue-500" style="background-color: #000; @error('addMenuItems') border-color: red; @enderror"/>
                     <span class="ml-2">Yes</span>
                 </label>
                 <label class="inline-flex items-center ml-4">
-                    <input type="radio" wire:model="addMenuItems" value="no" class="form-radio text-blue-500" style="@error('addMenuItems') border-color: red; @enderror"/>
+                    <input type="radio" wire:model="addMenuItems" value="no" class="form-radio text-blue-500" style="background-color: #000 @error('addMenuItems') border-color: red; @enderror"/>
                     <span class="ml-2">No</span>
                 </label>
                 @error('addMenuItems') <span style="color: red; display: block;">{{ $message }}</span> @enderror
@@ -31,20 +31,20 @@
             @foreach ($menuItems as $index => $menuItem)
                 <div class="mb-4" wire:key="menuItem-{{ $index }}">
                     <div class="mb-2">
-                        <label for="menuItem[{{ $index }}]" class="block text-sm font-medium text-gray-700">Menu Item</label>
-                        <input type="text" id="menuItem[{{ $index }}]" wire:model="menuItem.{{ $index }}" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm" style="@error("menuItem.{$index}") border-color: red; @enderror"/>
+                        <label for="menuItem[{{ $index }}]" class="block text-sm font-medium text-white">Menu Item</label>
+                        <input type="text" id="menuItem[{{ $index }}]" wire:model="menuItem.{{ $index }}" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm" style="background-color: #000; @error("menuItem.{$index}") border-color: red; @enderror"/>
                         @error("menuItem.{$index}") <span class="text-red-500 text-xs">{{ $message }}</span> @enderror
                     </div>
 
                     <div class="mb-2">
-                        <label for="itemPrice[{{ $index }}]" class="block text-sm font-medium text-gray-700">Price</label>
-                        <input type="number" id="itemPrice[{{ $index }}]" wire:model="itemPrice.{{ $index }}" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm" />
+                        <label for="itemPrice[{{ $index }}]" class="block text-sm font-medium text-white">Price</label>
+                        <input type="number" id="itemPrice[{{ $index }}]" wire:model="itemPrice.{{ $index }}" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm" style="background-color: #000;"/>
                         @error("itemPrice.{$index}") <span class="text-red-500 text-xs">{{ $message }}</span> @enderror
                     </div>
 
                     <div class="mb-2">
-                        <label for="menuImage[{{ $index }}]" class="block text-sm font-medium text-gray-700">Menu Image</label>
-                        <input type="file" id="menuImage[{{ $index }}]" wire:model="menuImage.{{ $index }}" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm" />
+                        <label for="menuImage[{{ $index }}]" class="block text-sm font-medium text-white">Menu Image</label>
+                        <input type="file" id="menuImage[{{ $index }}]" wire:model="menuImage.{{ $index }}" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm"  style="background-color: #000;"/>
                         @error("menuImage.{$index}") <span class="text-red-500 text-xs">{{ $message }}</span> @enderror
                     </div>
                 </div>
